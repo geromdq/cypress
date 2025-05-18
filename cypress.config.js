eeca8352fe0de12bd3e79f90b3b1a55fa127379b
Gero <gero295@gmail.com>
2025-05-18 19:40:27 -0300

    Agregado test API 200

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      baseUrl: 'https://www.superprof.com.ar'
       // implement node event listeners here
     },
+    
   },
 });
--- cypress/e2e/homePageTests.cy.js
+++ cypress/e2e/homePageTests.cy.js
@@ -1,16 +1,27 @@
-import superprofHome from "../pages/superprofHome"
-import superprofDisplay from "../pages/superprofResults"
+import superprofHome from "../PageObjectModel/pages/superprofHome"
+import superprofResults from "../PageObjectModel/pages/superprofResults"
 
 const home = new superprofHome
-const display = new superprofResults 
+const results = new superprofResults 
 describe('.. ',()=>{
 
     beforeEach("Navigate to home",()=>{
         home.navigateToHome()
     })
 
+
+    //Front end tests
     it("Find teacher",()=>{
         home.search('Clases de ingles')
-        display.mainTitle().should('have.text','Los profesores de inglés cerca de ti')
+        results.mainTitle().should('have.text','Los profesores de inglés cerca de ti')
     })
+
+
+    //API tests
+    it("verify 200 code on homePage",()=>{
+        cy.intercept('GET','/').as('getHome')
+        home.navigateToHome()
+        cy.wait('@getHome').its('response.statusCode').should('eq', 200);
+    })
+
 })
